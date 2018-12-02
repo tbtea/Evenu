8e64be7a573acda7d7d64116a41211be55b357ca
Tony Tea <tbtea@ucalgary.ca>
2018-12-02 11:03:12 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tonytea/evenu/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.tonytea.evenu;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseAuthException;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.login_email);
+        mPasswordView = (EditText) findViewById(R.id.login_password);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+               if (actionId == R.integer.login || actionId == EditorInfo.IME_NULL){
+                   //attemptLogin();
+                   return true;
+               }
+                return false;
+            }
+        });
+
+        // Grab instance of Firebase.Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signInUser(View v){
+        // calls attemptLogin()
+        attemptLogin();
+    }
+
+    public void registerUser(View v){
+        //Opens up registration page
+        Intent intent = new Intent(this, com.example.tonytea.evenu.RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    private void attemptLogin() {
+
+        String emailAddr = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (emailAddr.isEmpty()) return;
+        Toast.makeText(this, "Logging in", Toast.LENGTH_SHORT).show();
+
+        mAuth.signInWithEmailAndPassword(emailAddr, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "SignInWithEmail() onComplete: " + task.isSuccessful());
+
+                if (!task.isSuccessful()){
+                    Log.d("Evenu", "There was a problem signing in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                }
+                else{
+                    // Intent intent = new Intent (this, ChatActivity.class);
+                    // finish();
+                    // startActivity(intent);
+                }
+            }
+        });
+
+
+    }
+
+    private void showErrorDialog(String message){
+        new AlertDialog.Builder(this).setTitle("oops").setMessage(message).setPositiveButton("ok",null).show();
+    }
+}
---
+++ app/src/main/java/com/example/tonytea/evenu/RegisterActivity.java
@@ -0,0 +1,150 @@
+package com.example.tonytea.evenu;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+    private EditText mConfirmPasswordView;
+
+    private FirebaseAuth mAuth;
+
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.register_email);
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.register_username);
+        mPasswordView = (EditText) findViewById(R.id.register_password);
+        mConfirmPasswordView = (EditText) findViewById(R.id.register_confirm_password);
+
+        mConfirmPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == R.integer.register_form_finished || actionId == EditorInfo.IME_NULL){
+                    attemptRegistration();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    // Signs up when sign-up button is pressed
+    public void signUp(View v){
+        attemptRegistration();
+    }
+
+    public void attemptRegistration(){
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)){
+            mPasswordView.setError(getString(R.string.invalid_password_error));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(email)){
+            mEmailView.setError(getString(R.string.error_enter_email));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)){
+            mEmailView.setError(getString(R.string.error_email_invalid));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel){
+            focusView.requestFocus();
+        } else{
+            createFirebaseUser();
+        }
+    }
+
+
+    private boolean isEmailValid(String email){
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password){
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length()>4;
+    }
+
+    public void createFirebaseUser(){
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("FlashChat", "createUser onComplete: " + task.isSuccessful());
+
+                        if(!task.isSuccessful()){
+                            Log.d("FlashChat", "user creation failed");
+                            showErrorDialog("Registration attempt failed");
+                        } else {
+                            saveDisplayName();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            finish();
+                            startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+    private void saveDisplayName() {
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences("Chat prefs", 0);
+        prefs.edit().putString("username", displayName).apply();
+    }
+
+
+    private void showErrorDialog(String message){
+
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+    }
+}
