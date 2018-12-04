6aa4097b426aa02466bb0cb2ffac53a358972183
Tony Tea <tbtea@ucalgary.ca>
2018-12-04 12:48:42 -0700

    - added the main event page(empty so far)recent changes

---
+++ app/src/main/java/com/example/tonytea/evenu/Flower.java
@@ -0,0 +1,13 @@
+package com.example.tonytea.evenu;
+
+public class Flower {
+    public String name;
+    public String imageLink;
+    public int amount;
+
+    public Flower(String name, String imageLink, int amount){
+        this.name = name;
+        this.imageLink = imageLink;
+        this.amount = amount;
+    }
+}
---
+++ app/src/main/java/com/example/tonytea/evenu/MainDisplayActivity.java
@@ -0,0 +1,15 @@
+package com.example.tonytea.evenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainDisplayActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page_activity);
+
+
+    }
+
+}
