2db219198e9442023bcd1207f9b7f5f763b3fddc
Bebo <rfiker@ucalgary.ca>
2018-12-05 09:23:22 -0700

    Keyboard Ready For Deployment
    Added the keyboard with all it's functionality. It'll just have to be attached to the right edittexts.

---
+++ app/src/main/java/com/example/tonytea/evenu/EventAdapter.java
@@ -0,0 +1,23 @@
+package com.example.tonytea.evenu;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewholder> {
+    @NonNull
+    @Override
+    public EventAdapter.EventViewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventAdapter.EventViewholder eventViewholder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
