c947d41dae91b769673058e22236b415bb0ec9ee
Tony Tea <tbtea@ucalgary.ca>
2018-12-06 23:05:13 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/main_page_activity.xml
    yes daddy

---
+++ app/src/main/java/com/example/tonytea/evenu/Comment.java
@@ -0,0 +1,34 @@
+package com.example.tonytea.evenu;
+
+public class Comment {
+
+    private String message;
+    private String author;
+    private String id;
+
+    public Comment(String message, String author, String i) {
+        this.message = message;
+        this.author = author;
+        this.id = i;
+    }
+
+    // no argument constructor
+    public Comment() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
